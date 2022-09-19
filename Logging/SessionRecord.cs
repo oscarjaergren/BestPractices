c9f621d4f63da4555d4c70bf5606816f984dbd87
Oscar Jargren <oscar.jargren@dyneval.com>
2022-09-19 14:46:23 +0100

    Add project files.

---
+++ Logging/SessionRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace Logging;
+
+#pragma warning disable CS8618
+
+public record SessionRecord
+{
+    public int Id { get; set; }
+
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    public Guid UniqueId { get; set; }
+
+    public string UserId { get; set; }
+
+    public int OrganisationId { get; set; }
+
+    public string Email { get; set; }
+
+    public bool IsOnlineMode { get; set; }
+}

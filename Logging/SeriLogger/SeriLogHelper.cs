55590183997a2b7af042ad1d7adea91bffdb71c4
oscar <oscar.jargren@gmail.com>
2022-09-22 21:05:39 +0100

    Added in SeriLog as a Logger

---
+++ Logging/SeriLogger/SeriLogHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Serilog;
+using Serilog.Debugging;
+using Serilog.Events;
+
+namespace Logging.SeriLogger;
+
+internal sealed class SeriLogHelper
+{
+    private static ILogger InitializeSerilog()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(writeTo => writeTo.Console())
+            .CreateLogger();
+
+        // self logging setting
+        //SelfLog.Enable(msg => File.AppendAllText("logs\\serilog_internallog.log", msg, Encoding.UTF8));
+
+        return Log.Logger;
+    }
+}
